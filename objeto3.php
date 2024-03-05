6998ef75b80bdf6b35df1673dad4e6d2e22ad968
mialavenant <162342994+mialavenant@users.noreply.github.com>
2024-03-05 08:24:47 -0800

    Add files via upload

---
+++ objeto3.php
@@ -0,0 +1,27 @@
+//objetos
+$artista03 = [
+    "nombre" => "arianagrande",
+    "edad" => 21,
+    "genero" => "femenino",
+   "nacimiento" => 26 junio 1993,
+    "generomusical" => "pop",
+    "altura" => 1.54cm,
+    "peso" => 48k,
+   "pais" => "boca raton florida estados unidos",
+    "grammyes" => 2,
+    "programas" => "victorius sam y cat",
+   "padres" => "joan Grande Edward Butera",
+   "exitos" => "into you 7 rings needy positions",
+   "hermanos" => "frankie Grande",
+   "pasatiempos" => "nadar leer escribir ir compras",
+   "mejor año" => 2010, 
+   "albumes" => "eternal sushine positions sweetner my Everything Dangerous Woman", 
+  "cancionfamosa" => "Side to side", 
+ "cancionfamosa" => "positions", 
+]
+$people = [
+    $artista01,
+    $artista02,
+    $artista03,
+];
+
